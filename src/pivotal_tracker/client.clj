6832dd505f02c94787c6ee21cbddbe2cef2673ae
Andy Lindeman <alindeman@gmail.com>
2013-04-11 15:59:02 -0400

    Retrieve a specific project

--- src/pivotal_tracker/client.clj
+++ src/pivotal_tracker/client.clj
@@ -3,10 +3,12 @@
             [pivotal-tracker.xml :as xml]))
 
 (defn- request [f token path]
-  (:body
-    (f (str "https://www.pivotaltracker.com/services/v3" path)
-       {:headers {"X-TrackerToken" token}}
-       {:accept :json})))
+  (println (str "Requesting " path))
+  (xml/simple-parse
+    (:body
+      (f (str "https://www.pivotaltracker.com/services/v3" path)
+         {:headers {"X-TrackerToken" token}}
+         {:accept :json}))))
 
 (defn- get-request [token path]
   (request client/get token path))
@@ -16,5 +18,8 @@
 (defn projects [token]
   (map :project
     (:projects
-      (xml/simple-parse
-        (get-request token "/projects")))))
+      (get-request token "/projects"))))
+
+(defn project [token id]
+  (:project
+    (get-request token (str "/projects/" id))))
