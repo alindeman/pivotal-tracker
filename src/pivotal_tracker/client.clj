a537f86ac834ecc5fb2cdfa88ea2e44496d47be3
Andy Lindeman <alindeman@gmail.com>
2013-04-11 16:09:03 -0400

    List of stories w/ filtering if you wish

--- src/pivotal_tracker/client.clj
+++ src/pivotal_tracker/client.clj
@@ -2,18 +2,20 @@
   (:require [clj-http.client :as client]
             [pivotal-tracker.xml :as xml]))
 
-(defn- request [f token path]
+(defn- request [f token path query-params]
   (println (str "Requesting " path))
   (xml/simple-parse
     (:body
       (f (str "https://www.pivotaltracker.com/services/v3" path)
-         {:headers {"X-TrackerToken" token}}
-         {:accept :json}))))
+         {:headers {"X-TrackerToken" token}
+          :query-params query-params}))))
 
-(defn- get-request [token path]
-  (request client/get token path))
 
-(defrecord Project [id])
+(defn- get-request
+  ([token path]
+    (get-request token path {}))
+  ([token path query-params]
+    (request client/get token path query-params)))
 
 (defn projects [token]
   (map :project
@@ -23,3 +25,11 @@
 (defn project [token id]
   (:project
     (get-request token (str "/projects/" id))))
+
+(defn stories
+  ([token project-id]
+    (stories token project-id {}))
+  ([token project-id query-params]
+    (map :story
+      (:stories
+        (get-request token (str "/projects/" project-id "/stories") query-params)))))
